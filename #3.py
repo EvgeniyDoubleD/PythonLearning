0b15cf7c24108c44f6b28ea5023e0137df8763d9
Evgeniy <ugin2308@mail.ru>
2022-04-25 22:03:41 +0300

    added #3

--- #3.py
+++ #3.py
@@ -1,6 +1,7 @@
 #вывести числа от -н до н
-
-num = int(input("vvedite N"))
-
-for i in range(-num, num+1):
+import math
+num = int(input("Введите число N: "))
+absNum = abs(num)
+print('Список от -N до N: ')
+for i in range(-absNum, absNum+1):
     print(i, end= ' ')
