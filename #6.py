54754a31e80476ab5b667429a7c3bb9130f70294
Evgeniy <ugin2308@mail.ru>
2022-04-25 22:47:55 +0300

    #6 done

--- #6.py
+++ #6.py
@@ -1,15 +1,24 @@
 
-
+# Дано число обозначающее день недели. Вывести его название и указать является ли он выходным.
 
 def try_parse_int():
     while True:
         try:
-            console_input = int(input("Enter a weekday number: "))
-            if console_input > 0:
+            console_input = int(input("Введите день недели: "))
+            if console_input>0 and console_input <=7:
                 return int(console_input)
         except ValueError:
-            print("Is not a valid number.")
+            print("Не подходящее значение")
 
 
-a = try_parse_int()
-print(a)
+day = try_parse_int()-1
+
+weekdays = ["Понедельник", "Вторник", "Среда",
+            "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+if weekdays[day] == 'Суббота':
+    print(f"Выходной день недели - {weekdays[day]}")
+elif weekdays[day] == 'Воскресенье':
+    print(f"Выходной день недели - {weekdays[day]}")
+else:
+    print(f"Будний день недели - {weekdays[day]}")
