20809132d390e75eb9e39f12f45c70ecddac803b
Evgeniy <ugin2308@mail.ru>
2022-04-26 22:23:34 +0300

    fixed

--- hello.py
+++ hello.py
@@ -4,3 +4,11 @@ if a2**2 == a1 or a2 == a1**2:
     print ("Kvadratom yavlaetsa")
 else:
     print ("NET")
+    
+    weekdays = ["Понедельник", "Вторник", "Среда",
+            "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+if weekdays[niceDay] == "Воскресенье" or "Суббота":
+    print(f"Выходной день недели - {weekdays[niceDay]}")
+else:
+    print(f"Будний день недели - {weekdays[niceDay]}")
