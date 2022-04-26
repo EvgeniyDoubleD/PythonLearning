ca4c2092a0b4d10ceb2bf9949fde374198cf764c
Evgeniy <ugin2308@mail.ru>
2022-04-26 22:22:48 +0300

    7 done perfectly xD

--- #7.py
+++ #7.py
@@ -1,4 +1,9 @@
-message = "Hello"
- 
-for c in message:
-    print(c)
+#Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+
+
+for x in range(0,2):
+    for y in range(0,2):
+        for z in range(0,2):
+            p = (not(x&y&z)) == (not(x))or(not(y))or(not(z))
+            print(f'for {x}, {y}, {z}: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z  --> {p}')
+            #print(((not(x&y&z))) == ((not(x))&(not(y))&(not(z))))
